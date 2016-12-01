0ddd3788160f7d92381edcba350292da02966b96
Mustaq Hussain <MustaqHussain@users.noreply.github.com>
2016-12-01 16:05:00 +0000

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,6 +1,10 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+// Create a server
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
+
+// 127.0.0.1 always the address of your local machine (same as localhost)
+// 1337      port where this app is listening (can be any free port)
 console.log('Server running at http://127.0.0.1:1337/');
